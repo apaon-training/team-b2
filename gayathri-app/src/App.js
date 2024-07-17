a743ce3001708a14ec1cb5b5eb3d98e6a82641c4
gayathrikuruva <k.gayathri.apaon@gmail.com>
2024-07-17 05:22:50 +0000

    gayathri changes

--- gayathri-app/src/App.js
+++ gayathri-app/src/App.js
@@ -3,21 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn gayathri demo project
-        </a>
-      </header>
+    <div className="App flex-column">
+      <div className='flex h-10rem'>
+        <div className='flex-column w-full bg-teal-500'>
+          Store Locator
+        </div>
+        <div className='flex-column w-5rem bg-teal-700'>
+          Avatar
+        </div>
+      </div>
+
+      <div className='flex h-full'>
+        <div className='flex-column w-30rem bg-pink-500'>
+          List Container
+        </div>
+        <div className='flex-column w-full bg-pink-600'>
+          Detail Container
+        </div>
+
+      </div>
+
     </div>
   );
 }
