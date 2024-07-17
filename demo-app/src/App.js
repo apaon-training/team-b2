05870a6eace8c25261c25fe53a13794969842d96
apaon-training <163280686+apaon-training@users.noreply.github.com>
2024-07-17 11:34:31 +0000

    fixed layout

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -3,22 +3,43 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React from Apaon
-        </a>
-      </header>
+    //Main container
+    <div className="App flex-column">
+      {/* Top navigation bar */}
+      <div className='flex h-10rem'>
+        {/* Title column */}
+        <div className='flex-column w-full bg-green-300'>
+             Store locator
+        </div>
+        {/* Avatar column */}
+        <div className='flex-column shadow-8  w-5rem bg-green-700'>
+             Avatar
+        </div>
+      </div> 
+      {/* Details container */}
+      <div className='flex h-full'>
+        {/* List container */}
+        <div className='flex-column w-30rem bg-green-300'>
+           List container
+        </div>  
+        {/* Details container */}
+        <div className='flex-column w-full bg-green-500'>
+          <div className='flex h-30rem'>
+            Map container
+          </div>
+        <div className='flex align-items-stretch flex-wrap h-20rem'>
+            {/* Store details */}
+            <div className='flex align-items-center w-6  justify-content-center bg-green-300'>
+                Store details
+            </div>
+            {/* Store address */}
+            <div className='flex align-items-center w-6  justify-content-center shadow-8  bg-green-700'>
+                Address
+            </div>
+      </div>
     </div>
+ </div> 
+</div>
   );
 }
 
--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import '../node_modules/primeflex/primeflex.css'
+import 'primeflex/themes/primeone-light.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
