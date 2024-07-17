9ceca65cd03d640079ddde2f329c8a3a890e61a4
Pahmida-apaon <p.pahmida.apaon@gmail.com>
2024-07-17 12:04:19 +0000

    changes

--- pahmida-app/src/App.js
+++ pahmida-app/src/App.js
@@ -3,22 +3,39 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React from Pahmida
-        </a>
-      </header>
+    <div className="App flex-column">
+     <div className='flex h-10rem'>
+      <div className='flex-column w-full bg-pink-200'>
+       store locator
+      </div>
+        <div className='flex-column w-5rem bg-pink-300'>
+            Avatar
+        </div>
     </div>
+
+     <div className='flex h-full'>
+      <div className='flex-column w-30rem bg-pink-400'>
+         list container
+      </div>
+      <div className='flex-column w-full bg-pink-500'>
+        <div className='flex h-25rem '>
+           Detail container
+         </div>
+         <div className='flex-column w-full bg-pink-600'>
+          <div className='flex h-20rem'>
+     <div className='flex h-20rem'>
+      <div className='flex-row w-30rem bg-pink-200'>
+       details
+      </div>
+        <div className='flex-row w-30rem bg-pink-300'>
+            phone number
+        </div>
+    </div>
+          </div>
+          </div>
+          </div>
+      </div>
+      </div>
   );
 }
 
--- pahmida-app/src/index.js
+++ pahmida-app/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '../node_modules/primeflex/primeflex.css';
+import 'primeflex/themes/primeone-light.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
