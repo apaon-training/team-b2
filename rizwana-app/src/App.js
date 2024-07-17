2fa6c2c060276971f686844b022a432ff043834a
Rizwana-apaon <s.rizwana.apaon@gmail.com>
2024-07-17 03:54:03 +0000

    changes done

--- rizwana-app/src/App.js
+++ rizwana-app/src/App.js
@@ -3,7 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App flex-column">
+      <div className='flex h-10rem'>
+        <div className='flex-column w-full bg-green-300'>
+             store locator
+        </div>
+        <div className='flex-column w-5rem bg-green-700'>
+          Avatar
+        </div>
+      </div>
+      <div className='flex h-full'>
+        <div className='flex-column w-30rem bg-green-300'>
+          List container
+        </div>
+        <div className='flex-column w-full bg-green-500'>
+          Detail container
+       </div>
+
+      </div>
     </div>
   );
 }
