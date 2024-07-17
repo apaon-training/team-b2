6c208d98fc6f7afb4e6732e44ca15233b5b362fc
Rizwana-apaon <s.rizwana.apaon@gmail.com>
2024-07-17 04:42:55 +0000

    changes  done

--- rizwana-app/src/App.js
+++ rizwana-app/src/App.js
@@ -5,18 +5,18 @@ function App() {
   return (
     <div className="App flex-column">
       <div className='flex h-10rem'>
-        <div className='flex-column w-full bg-green-300'>
+        <div className='flex-column w-full bg-pink-500'>
              store locator
         </div>
-        <div className='flex-column w-5rem bg-green-700'>
+        <div className='flex-column w-5rem bg-teal-600'>
           Avatar
         </div>
       </div>
       <div className='flex h-full'>
-        <div className='flex-column w-30rem bg-green-300'>
+        <div className='flex-column w-30rem bg-purple-400'>
           List container
         </div>
-        <div className='flex-column w-full bg-green-500'>
+        <div className='flex-column w-full bg-indigo-100'>
           Detail container
        </div>
 
