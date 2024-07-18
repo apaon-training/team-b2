5893986d3b49b1f7074e6e0011dcfeed7342ea21
gayathrikuruva <k.gayathri.apaon@gmail.com>
2024-07-18 09:39:20 +0000

    modified changes

--- gayathri-app/src/App.js
+++ gayathri-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App flex-column">
       <div className='flex h-10rem'>
-        <div className='flex-column text-black-alpha-90 font-bold w-full bg-pink-500'>
+        <div className='flex-column text-indigo-900 font-bold w-full bg-pink-400'>
           Store Locator
         </div>
         <div className='flex-column text-white-alpha-90 font-bold w-5rem bg-pink-300'>
@@ -17,15 +17,15 @@ function App() {
         <div className='flex-column text-white-alpha-90 font-bold w-30rem bg-pink-800'>
           List Container
         </div>
-        <div className='flex-column text-white-alpha-90 text-right font-bold w-full bg-pink-900'>
+        <div className='flex-column text-white-alpha-90 text-right font-bold w-full bg-green-600'>
         <div className='flex h-30rem'>
           Map container 
         </div>
-        <div className='flex align-items-stretch flex-wrap column-gap-8 h-15rem'>
-        <div className='flex w-4 justify-content-center text-white-alpha-90 font-bold bg-indigo-700'>
+        <div className='flex align-items-stretch flex-wrap h-15rem'>
+        <div className='flex w-6 justify-content-center text-white-alpha-90 font-bold bg-indigo-700'>
           Store Timings
           </div>
-        <div className='flex w-7 justify-content-center text-white-alpha-90 font-bold bg-indigo-700'>
+        <div className='flex w-6 justify-content-center text-white-alpha-90 font-bold bg-indigo-800'>
           Store Contact Details
           </div>
       </div>
