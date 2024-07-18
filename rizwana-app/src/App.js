e74391d486d56ea49f26240dd676d2fa9fc0aa10
Rizwana-apaon <s.rizwana.apaon@gmail.com>
2024-07-18 10:56:32 +0000

    changes done

--- rizwana-app/src/App.js
+++ rizwana-app/src/App.js
@@ -1,26 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import { Avatar } from 'primereact/avatar';
+import { InputText } from 'primereact/inputtext';
 
 function App() {
   return (
     <div className="App flex-column">
-      <div className='flex h-10rem'>
-        <div className='flex-column w-full bg-pink-500'>
+      <div className='flex justify-content-center h-8rem navigation-bar'>
+        <div className='flex align-items-center justify-content-center text-7xl store-container w-full '>
              store locator
         </div>
-        <div className='flex-column shadow-8 w-5rem bg-teal-600'>
+        <div className='flex  align-items-center justify-content-center mr-3  w-5rem '>
         <Avatar label="SR" size="xlarge" shape="circle" />
         </div>
       </div>
       <div className='flex h-full'>
         <div className='flex-column w-30rem bg-purple-400'>
-          List container
+        <InputText value={"rizu"}  />
         </div>
         <div className='flex-column w-full bg-orange-200'>
            <div className='flex h-25rem'>
               <div className='flex-column w-full bg-indigo-100'>   
-        Detail container</div>
+        Detail container</div>  
         </div>
         <div className='flex h-20rem'>
         <div className='flex-column w-6 bg-yellow-300'>
