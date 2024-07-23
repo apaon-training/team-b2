f66742dc30b186208ba79ae11b6509f9566ba4c5
saisree-apaon <v.saisree.apaon@gmail.com>
2024-07-23 12:05:42 +0000

    changes

--- saisree-app/src/Login/ Login.js
+++ saisree-app/src/Login/ Login.js
@@ -1,13 +1,28 @@
 function Login() {
     return (
         <>
-        <span> Store locator Login </span><br></br>
+        <div className=" Login flex-column"> 
+          <div className='flex justify-content-center h-10rem text-5xl'>
+          <div className='flex align-items-center justify-content-center w-full'>
+            Store locator Login </div>
+            </div>
+        <div className=" Login flex-column"></div>
+        <div className='flex justify-content-center text-5xl'>   
+        <div className='flex align-items-center justify-content-center text-5xl'>
         <label class="Username"> Username:</label>
         <input type="text" id="username"/><br></br>
+        </div>
+        
+        
         <label class="Password"> Password:</label>
-        <input type="text" id="password"/>
+        <input type="text" id="password"/><br></br>
+        </div>
+       <div className='flex justify-content-center text-5xl'>
+        <div className='flex align-items-center justify-content-center'>
         <button type="button">submit</button>
-        <button type="button"> cancel</button>
+        <button type="button"> cancel</button></div>
+        </div>
+        </div>
         </>
     );
 }
