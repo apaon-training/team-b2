40a03496589ad628d77746f44545c94982d95ca6
anitha-apaon <anitha.apaon@gmail.com>
2024-07-23 09:33:03 +0000

    new changes

--- anitha-app/src/App.js
+++ anitha-app/src/App.js
@@ -1,4 +1,4 @@
-
+import logo from './logo.svg';
 import './App.css';
 import {Avatar} from 'primereact/avatar';
 
@@ -6,37 +6,34 @@ function App() {
   return (
     <div className="App flex-column">
       <div className='flex justify-content-center navigation-bar h-8rem'>
-        <div className='flex align-items-center justify-content-center text-6xl text-white-alpha-90 w-full '>
-           Store Locator
+        <div className='flex align-items-center justify-content-center store-locator-container text-6xl text-white-alpha-90 w-full'>
+          Store Locator
         </div>
-        <div className='flex align-items-center justify-content-center w-5rem '>
-        <Avatar label="P"size="xlarge" shape="circle" className='mr-3'/>
-       </div>
-       </div>
-
-  <div className='flex h-auto'>
-   <div className='flex-column w-30rem bg-yellow-300'>
-    List Container 
-    </div>
-    <div className='flex-column w-full bg-cyan-500'>
-      <div className='flex h-30rem'>
-        Map Container
+        <div className='flex align-items-center justify-content-center w-5rem'>
+          <Avatar label="VA" size="xlarge" shape="circle" className='mr-3'/>
+        </div>
       </div>
-      <div className='flex-align-items-stretch flex-wrap h-15rem'>
-        <div className='flex w-6 justify-content-center bg-purple-300'>
-          Store Details
+      <div className='flex h-auto'>
+        <div className='flex-column text-white-alpha-90 font-bold w-30rem bg-green-100'>
+          List Container
         </div>
-        <div className='flex w-6 justify-content-center bg-purple-300'>
-          Address
+        <div className='flex-column text-white-alpha-90 text-right font-bold w-full bg-pink-600'>
+        <div className='flex h-30rem'>
+          Map container 
         </div>
-     </div>
+        <div className='flex align-items-stretch flex-wrap h-15rem'>
+        <div className='flex w-6 justify-content-center text-white-alpha-90 font-bold bg-purple-500'>
+          Store Timings
+          </div>
+        <div className='flex w-6 justify-content-center text-white-alpha-90 font-bold bg-blue-400'>
+          Store Contact Details
+          </div>
+      </div>
     </div>
-
   </div>
+ </div>
 
-  </div>
   );
 }
-
 
 export default App;
--- anitha-app/src/index.js
+++ anitha-app/src/index.js
@@ -5,7 +5,7 @@ import App from  './App'
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/primeflex/primeflex.css';
 import 'primeflex/themes/primeone-light.css';
-import {PrimeReactProvider,PrimeReactContext}from 'primereact/api';
+import { PrimeReactProvider,PrimeReactContext } from 'primereact/api';
 import 'primeicons/primeicons.css';
 import 'primereact/resources/primereact.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
