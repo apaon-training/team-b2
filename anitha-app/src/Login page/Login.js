a0cd9ede60165a3da45094ddde95c96cb853ac33
anitha-apaon <anitha.apaon@gmail.com>
2024-07-23 12:18:06 +0000

    new chages

--- anitha-app/src/Login page/Login.js
+++ anitha-app/src/Login page/Login.js
@@ -1,9 +1,23 @@
-
+import './Login.css';
 function Login(){
     return(
-<>
-<span>Store Located Login</span>
-</>
+        <div className="flex-column h-screen bg-primary justify-content-center">
+            <div className='flex h-'>
+                Store Locator
+                </div>
+                <div className="flex">
+                    <div className=""
+                    Username:
+                    </div>
+                    <div className=""
+                    <input></input>
+                    </div>
+                    </div>
+                    <div className='flex align-items-center justify-content-center my-3 gap-5'>
+                        <button className=''
+              
+</div>
+</div>
 
     );
 }
