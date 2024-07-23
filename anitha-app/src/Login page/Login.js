eaf65b89472e57b64490d704e518f70744fcde3b
anitha-apaon <anitha.apaon@gmail.com>
2024-07-23 10:51:51 +0000

    new changes

---
+++ anitha-app/src/Login page/Login.js
@@ -0,0 +1,10 @@
+
+function Login(){
+    return(
+<>
+<span>Store Located Login</span>
+</>
+
+    );
+}
+export default Login;
--- anitha-app/src/index.js
+++ anitha-app/src/index.js
@@ -9,12 +9,12 @@ import { PrimeReactProvider,PrimeReactContext } from 'primereact/api';
 import 'primeicons/primeicons.css';
 import 'primereact/resources/primereact.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
-
+import Login from './Login page/Login';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <PrimeReactProvider>
-    <App />
+    <Login />
     </PrimeReactProvider>
   </React.StrictMode>
 );
