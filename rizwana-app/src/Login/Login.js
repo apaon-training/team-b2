06685fe4f4ac18da50cb26d06eda47163403914b
Rizwana-apaon <s.rizwana.apaon@gmail.com>
2024-07-23 11:38:49 +0000

    changes done

--- rizwana-app/src/Login/Login.js
+++ rizwana-app/src/Login/Login.js
@@ -4,15 +4,24 @@ import './Login.css';
 function Login() {
     return(
         <>
-        <span>Store Locator Login</span><br></br>
+         <div className="Login flex-column">
+      <div className='flex justify-content-center '>
+        <div className='flex align-items-center justify-content-center text-5xl  w-full '>
+        Store Locator Login
+        </div><br></br>
+        <div className='flex  align-items-center justify-content-center text-3xl'>
         <label class="username"> username:</label>
+        </div>
+        
         <input type="text" id="User"/><br></br>
+        
         <label class="password"> password:</label>
         <input type="text" id="Password"/><br></br>
         <button type="button" onclick="checkPassword()">Cancel</button>
         <button type="button" onclick="checkPassword()">Submit</button>
 
-
+        </div>
+        </div>
 
         </>
     );
