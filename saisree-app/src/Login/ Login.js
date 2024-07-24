1082ec8e5b87708142e0e91526b18a5519db746d
saisree-apaon <v.saisree.apaon@gmail.com>
2024-07-24 12:02:50 +0000

    changes done

--- saisree-app/src/Login/ Login.js
+++ saisree-app/src/Login/ Login.js
@@ -1,5 +1,32 @@
 import './ Login.css';
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
+import { useState } from 'react';
+
 function Login() {
+
+    const [usertext, setUserText] = useState();
+    const [passtext, setPassText] = useState();
+
+    const Credentials = {
+        user:'Admin',
+        password:'apaon'
+    }
+
+    const setUsername=(value)=>{
+        setUserText(value);
+    }
+    const setPassword=(value)=>{
+        setPassText(value);
+    }
+
+    const submitclicked=()=>{
+        if(Credentials.user === usertext && Credentials.password ===passtext){
+            console.log('Login successful !');
+        }else{
+            console.log('Login failed !');
+        }        
+     }    
     return (
         <>
         {/*title*/}
@@ -15,7 +42,9 @@ function Login() {
                 Username:
             </div>
             <div className="flex h-2rem">
-                <input></input>
+                {/*<input></input>*/}
+                <InputText onChange={(e) => setUsername(e.target.value)} />
+
              </div>
         </div>
 
@@ -26,7 +55,8 @@ function Login() {
                 Password:
             </div>
             <div className='flex h-2rem ml-2'>
-                <input></input>
+                {/*<input></input>*/}
+                <InputText onChange={(e) => setPassword(e.target.value)} />
              </div>
         </div>
 
@@ -34,14 +64,18 @@ function Login() {
         {/*button*/}
         <div>
             <div className='flex align-items-center justify-content-center gap-3 text-3xl'>
-            <button className='ccolor'>Cancel</button>
-            <button className='scolor'>Submit</button>
-            </div>
-        </div>
+            {/*<button className=>Cancel</button>
+            <button className=>Submit</button>*/}
+            
+            
+            <Button label="Cancel" severity="warning" raised />
+            <Button label="Submit" severity="success" raised onClick={() => submitclicked()} />
+         </div>
 
         {/*end*/}
         <div className='flex align-items-center justify-content-center my-3 gap-5 h-12rem text-lg'>
             <span>@copyright developed by apaon.com.au - B2 batch</span>
+        </div>
         </div>
         </div>
         </>
