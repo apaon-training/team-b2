7a3622058d9e71bc379aea0a29058d3e7fec20ff
anitha-apaon <anitha.apaon@gmail.com>
2024-07-24 11:59:12 +0000

    added

--- anitha-app/src/Login /Login.js
+++ anitha-app/src/Login /Login.js
@@ -1,4 +1,26 @@
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
+import { useState } from 'react';
 function Login() {
+    const [userText, setUserText ] = useState();
+    const [passText, setPassText ] = useState();
+    const credentials = {
+        user: 'Admin',
+        password: 'Anitha',
+    }
+    const setUserName = (value)=>{
+        setUserText(value);
+    }
+    const setPassword = (value)=>{
+        setPassText(value);
+    }
+    const submitClicked = () =>{
+        if(credentials.user === userText && credentials.password == passText){
+            console.log('Login successful !');
+        }else{
+            console.log('Login failed !');
+        }
+    }
     return (
         <>
         <div className="flex-column h-screen bg-primary">
@@ -12,24 +34,20 @@ function Login() {
                 User :
             </div>
             <div className="">
-                <input></input>
-            </div>
+                <InputText onChange={(e) => setUserName(e.target.value)} />           
+                 </div>
             </div>
             <div className="flex align-items-center justify-content-center text-2xl text-white gap-3 my-3">
             <div className="">
                 Password :
             </div>
             <div className="">
-                <input></input>
+                <InputText onChange={(e) => setPassword(e.target.value)} />    
             </div>
             </div>
             <div className="flex align-items-center justify-content-center my-3 gap-5 ">
-            <button className="bg-gray-200 text-white text-2xl">
-                Cancel 
-            </button>
-            <button className="bg-blue-700 text-white text-2xl">
-                Submit 
-            </button>
+            <Button label="Cancel" serverity="warning" raisrd />
+            <Button label="Submit" serverity="success" raised onClick={() => submitClicked()} />
             </div>
             <div className="flex align-items-center justify-content-center text-5m text-white gap-5 h-25rem">
                 <span>
