f3262048f21dccffd68236407aca630be59355b1
anitha-apaon <anitha.apaon@gmail.com>
2024-07-24 09:36:29 +0000

    changes

---
+++ anitha-app/src/Login /Login.js
@@ -0,0 +1,42 @@
+function Login() {
+    return (
+        <div className="flex-column h-screen bg-primary">
+            <div className="flex h-15rem align-items-center justify-content-center text-6xl text-white">
+            <span>
+            Store Locator Login
+            </span>
+            </div>
+            <div className="flex align-items-center justify-content-center text-3xl text-white gap-7 md:h-1rem">
+            <div className="">
+                User :
+            </div>
+            <div className="">
+                <input></input>
+            </div>
+            </div>
+            <div className="flex align-items-center justify-content-center text-3xl text-white gap-3 md:h-5rem">
+            <div className="">
+                Password :
+            </div>
+            <div className="">
+                <input></input>
+            </div>
+            </div>
+            <div className="flex align-items-center justify-content-center gap-5 md:h-5rem">
+            <button className="bg-pink-400 text-white text-2xl">
+                Cancel 
+            </button>
+            <button className="bg-green-600 text-white text-2xl">
+                Submit 
+            </button>
+            </div>
+            <div className="flex align-items-center justify-content-center text-sm text-white gap-5 md:h-28rem">
+                <span>
+                    @copyright developed by apaon.com.au - B2 batch
+                </span>
+            </div>
+            </div>
+     
+    );
+}
+export default Login;
--- anitha-app/src/index.js
+++ anitha-app/src/index.js
@@ -9,7 +9,7 @@ import { PrimeReactProvider,PrimeReactContext } from 'primereact/api';
 import 'primeicons/primeicons.css';
 import 'primereact/resources/primereact.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
-import Login from './Login page/Login';
+import Login from './Login/Login';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
