95f558728bbbd5e2cc37838d5d63d8e36f171927
Rizwana-apaon <s.rizwana.apaon@gmail.com>
2024-07-24 04:14:05 +0000

    changes done

--- rizwana-app/src/Login/Login.js
+++ rizwana-app/src/Login/Login.js
@@ -4,24 +4,38 @@ import './Login.css';
 function Login() {
     return(
         <>
-         <div className="Login flex-column">
-      <div className='flex justify-content-center '>
-        <div className='flex align-items-center justify-content-center text-5xl  w-full '>
-        Store Locator Login
+         <div className="flex-column  h-full">
+        <div className='flex align-items-center justify-content-center text-6xl my-3  h-15rem '>
+        <div>Store Locator Login</div>
         </div><br></br>
-        <div className='flex  align-items-center justify-content-center text-3xl'>
-        <label class="username"> username:</label>
+        <div className='flex  align-items-center justify-content-center  text-3xl my-3 gap-7'>
+        <div className="">
+            User:
+        </div>
+        <div className="">
+            <input></input>
+        </div>
         </div>
         
-        <input type="text" id="User"/><br></br>
-        
-        <label class="password"> password:</label>
-        <input type="text" id="Password"/><br></br>
-        <button type="button" onclick="checkPassword()">Cancel</button>
-        <button type="button" onclick="checkPassword()">Submit</button>
-
+        <div className="flex  align-items-center justify-content-center text-3xl my-3 gap-2 ">
+        <div className="">
+            Password:
         </div>
+        <div className="">
+            <input></input>
         </div>
+       </div>
+       <div>
+        <div className="flex  align-items-center justify-content-center  text-3xl my-3 gap-3">
+        <button className="cancel">Cancel</button>
+        <button className="submit">Submit</button>
+        </div>
+       </div>
+       <div className="flex  align-items-center justify-content-center text-3xl my-3 gap-5 h-12rem text-sm">
+        <span>@copyright developed by apaon.com.au - B2 batch</span>
+       </div>
+       </div>
+    
 
         </>
     );
