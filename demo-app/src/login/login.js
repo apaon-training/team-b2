e4063add514c48e43887fbbfa326c2660bf5b4d7
apaon-training <163280686+apaon-training@users.noreply.github.com>
2024-07-24 12:01:03 +0000

    added login logic

--- demo-app/src/login/login.js
+++ demo-app/src/login/login.js
@@ -1,6 +1,46 @@
-
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
+import { useState } from 'react';
 
 function Login(){
+
+    //declare 'user' variable
+    const [userText, setUserText ] = useState();
+    const [passText, setPassText ] = useState();
+
+    //Hook 3 functions for Username & Password Input - Button click
+
+   // Step 1: Store user name into variable
+   // Step 2: Store password into variable
+
+   //Step 3: On clicking on 'Submit' button -> Verify the saved username and password and then show alert
+    const credentials = {
+        user: 'Admin',
+        password: 'apaon'
+    }
+
+    const setUserName = (value)=>{
+        // console.log(value);
+        setUserText(value);  // saving value into 'userText' variable
+    }
+
+    const setPassword = (value)=>{
+        // console.log(value);
+        setPassText(value); // saving value into 'passText' variable
+    }
+
+    const submitClicked =() =>{
+        // console.log('User name : ' + userText);
+        // console.log('Password : ' + passText);
+        if(credentials.user === userText && credentials.password == passText){
+            //Login successful
+            console.log('Login successful !');
+        }else{
+            //Login failed.
+            console.log('Login failed !');
+        }
+    }
+
     return (
         <>
         <div className="flex-column h-screen bg-primary">
@@ -17,7 +57,8 @@ function Login(){
                     Username:
                 </div>
                 <div className="">
-                    <input></input>
+                    {/* <input></input> */}
+                    <InputText onChange={(e) => setUserName(e.target.value)} />
                 </div>
             </div>
 
@@ -27,18 +68,21 @@ function Login(){
                     Password:
                 </div>
                 <div className="">
-                    <input></input>
+                    {/* <input></input> */}
+                    <InputText onChange={(e) => setPassword(e.target.value)} />
                 </div>
             </div>
 
             {/* Submit & Cancel */}
              <div className="flex align-items-center justify-content-center my-3 gap-5">
-                <button className="">
+                {/* <button className="">
                     Cancel:
                 </button>
                 <button className="">
                     Submit:
-                </button>
+                </button> */}
+                <Button label="Cancel" severity="warning"   raised />
+                <Button label="Submit"  severity="success"  raised  onClick={() => submitClicked()} />
             </div>
 
             {/* copyright footer */}
