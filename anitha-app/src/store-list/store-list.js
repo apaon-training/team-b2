d420164468366089971595f1b9f60866fdb2d8b8
anitha-apaon <anitha.apaon@gmail.com>
2024-07-30 10:20:46 +0000

    added

--- anitha-app/src/store-list/store-list.js
+++ anitha-app/src/store-list/store-list.js
@@ -27,12 +27,12 @@ function StoreList() {
         "storeDetails":"D-mart",
         "sttoreTimings":[
                           "Mon-Fri 9 to 10pm",
-                          "Sat-Sun 9 to 05pm",
+                          "Sat-Sun 9 to 05pm"
 
         ],
         "storeAddress":{
             "phone":"+61 224 445 665",
-            "address":"54-76-sri rama nagar",
+            "address":"54-76-sri rama nagar"
         },
         "storeMapLocation":{
             "latitute":65.657,
@@ -45,12 +45,12 @@ function StoreList() {
         "storeDetails":"Jyothi mall",
         "sttoreTimings":[
                           "Mon-Fri 9 to 8pm",
-                          "Sat-Sun 9 to 06pm",
+                          "Sat-Sun 9 to 06pm"
 
         ],
         "storeAddress":{
             "phone":"+61 665 678 987",
-            "address":"54-54 ganesh nagar",
+            "address":"54-54 ganesh nagar"
         },
         "storeMapLocation":{
             "latitute":34-378,
@@ -63,12 +63,12 @@ function StoreList() {
         "storeDetails":"Digital",
         "sttoreTimings":[
                           "Mon-Fri 8 to 10pm",
-                          "Sat-Sun 8 to 05pm",
+                          "Sat-Sun 8 to 05pm"
 
         ],
         "storeAddress":{
             "phone":"+61 276 487 564",
-            "address":"54-76-laksmi nagar",
+            "address":"54-76-laksmi nagar"
         },
         "storeMapLocation":{
             "latitute":65.748,
