c2fe5ff4748c8fe64c68d571c337d33f75997750
gayathrikuruva <k.gayathri.apaon@gmail.com>
2024-08-01 15:10:10 +0000

    update changes

--- gayathri-app/src/Home/home.js
+++ gayathri-app/src/Home/home.js
@@ -20,24 +20,24 @@ function Home(props) {
         <div className='flex align-items-center justify-content-center store-locator-container text-6xl w-full'>
           Store Locator
         </div>
-        <div className='flex align-items-center justify-content-center w-10rem'>
+        <div className='flex align-items-center justify-content-center gap-3 w-11rem'>
         <Button label="Logout" severity="danger" raised  size='small' onClick={() => onLogoutClicked ()} />
           <Avatar label="BA" size="xlarge" shape="circle" className='mr-3'/>
         </div>
       </div>
       <div className='flex h-auto'>
-        <div className='flex-column justify-content-center align-items-center text-white-alpha-90 font-bold w-30rem bg-pink-400'>
+        <div className='flex-column justify-content-center align-items-center text-white-alpha-90 font-bold w-30rem bg-orange-200'>
           <StoreList selectedStore={ (value) => setSelectedStoreObj (value) }/>
         </div>
-        <div className='flex-column text-white-alpha-90 text-right font-bold w-full bg-green-600'>
+        <div className='flex-column text-white-alpha-90 text-right w-full bg-green-600'>
         <div className='flex h-30rem'>
           Map container 
         </div>
-        <div className='flex align-items-stretch flex-wrap h-15rem'>
-        <div className='flex w-6 justify-content-center align-items-center text-white-alpha-90 font-bold bg-indigo-700'>
+        <div className='flex h-15rem'>
+        <div className='flex w-6 justify-content-center align-items-center text-blue-800 font-bold text-xl bg-white'>
           <StoreTimings storeObj={selectedStoreObj}/>
           </div>
-        <div className='flex w-6 justify-content-center align-items-center text-white-alpha-90 font-bold bg-indigo-800'>
+        <div className='flex w-6 justify-content-center align-items-center text-black-alpha-90 font-bold text-xl bg-white'>
           <StoreContact storeObj={selectedStoreObj}/>
           </div>
       </div>
--- gayathri-app/src/store-list/store-list.js
+++ gayathri-app/src/store-list/store-list.js
@@ -9,13 +9,13 @@ function StoreList(props) {
         "id":1001,
         "storeName": "Jyothi Mall",
         "storeDetails": "Groceries",
-        "storeTimings": ["mon-Fri-9 to 10 pm", 
-            "sat-Sun-9 to 5 pm"
+        "storeTimings": ["Mon-Fri-9 to 10 pm", 
+            "Sat-Sun-9 to 5 pm"
 
         ],
         "storeContact": {
-            "phone": "+81 123 145 165",
-            "address": "22-86-wollongong main road"
+            "phone": "Phone: +81 123 145 165",
+            "address": "Address: 22-86-wollongong main road"
         },
         "storeMapLocation": {
             "latitude":54.0378,
@@ -25,15 +25,87 @@ function StoreList(props) {
 
     {
         "id":1002,
-        "storeName": "City Square Mall",
-        "storeDetails": "Shopping and Entertainment",
-        "storeTimings": ["mon-Fri-10 to 10 pm", 
-            "sat-Sun-11 to 10 pm"
+        "storeName": "Dress Circle",
+        "storeDetails": "Saree Retailers",
+        "storeTimings": ["Mon-Fri-10 to 10 pm", 
+            "Sat-Sun-11 to 9 pm"
 
         ],
         "storeContact": {
-            "phone": "+06 303 801 585",
-            "address": "40-37-M-Bus Stand road"
+            "phone": "Phone: +08 886 989 992",
+            "address": "Address: 40-320C-Abdullah Khan Estate"
+        },
+        "storeMapLocation": {
+            "latitude":38.2134,
+            "longitude":89.9604
+        }
+    },
+
+    {
+        "id":1003,
+        "storeName": "Joyalukkas",
+        "storeDetails": "Jewellery Store",
+        "storeTimings": ["Mon-Fri-10:30 to 8:30 pm", 
+            "Sat-Sun-11 to 8 pm"
+
+        ],
+        "storeContact": {
+            "phone": "Phone: +08 518 289 888",
+            "address": "Address: 40-29-SP Square Park road"
+        },
+        "storeMapLocation": {
+            "latitude":99.2548,
+            "longitude":66.9632
+        }
+    },
+
+    {
+        "id":1004,
+        "storeName": "Foresta",
+        "storeDetails": " Cafe and Restaurant",
+        "storeTimings": ["Mon-Fri-11 to 11 pm", 
+            "Sat-Sun-12 to 12 pm"
+
+        ],
+        "storeContact": {
+            "phone": "Phone: +91 100 549 951",
+            "address": "Address: Bangarpet Road"
+        },
+        "storeMapLocation": {
+            "latitude":87.7896,
+            "longitude":78.6897
+        }
+    },
+
+    {
+        "id":1005,
+        "storeName": "Polar Bear",
+        "storeDetails": "Ice Cream Parlour",
+        "storeTimings": ["Mon-Fri-11 to 11:30 pm", 
+            "Sat-Sun-12 to 11:30 pm"
+
+        ],
+        "storeContact": {
+            "phone": "Phone: +09 741 130 033",
+            "address": "Address: Nandyal Check Post-Sree Rama Nagar"
+        },
+        "storeMapLocation": {
+            "latitude":81.3456,
+            "longitude":91.7890
+        }
+    },
+
+    {
+        "id":1006,
+        "storeName": "City Square",
+        "storeDetails": "Shopping and Entertainment",
+        "storeTimings": ["Mon-Fri-10 to 10 pm", 
+            "Sat-Sun-11 to 10 pm"
+
+        ],
+        "storeContact": {
+            "phone": "Phone: +06 303 801 585",
+            "address": "Address: 40-37-M-Bus Stand road"
         },
         "storeMapLocation": {
             "latitude":12.4567,
@@ -43,37 +115,21 @@ function StoreList(props) {
 
 
        {
-        "id":1003,
+        "id":1007,
         "storeName": "Big Bazar",
         "storeDetails": "Appliances",
-        "storeTimings": ["mon-Fri-9 to 8 pm", 
-            "sat-Sun-10 to 6 pm"
+        "storeTimings": ["Mon-Fri-9 to 8 pm", 
+            "Sat-Sun-10 to 6 pm"
         ],
         "storeContact": {
-            "phone":"+61 987 765 543",
-            "address":"33-98-chenamma circle"
+            "phone":"Phone: +61 987 765 543",
+            "address":"Address: 33-98-chenamma circle"
         },
         "storeMapLocation":{
             "latitude":86.0435,
             "longitude":68.0398
         }
         
-    },
-        {
-        "id":1004,
-        "storeName": "Gayathri Mall",
-        "storeDetails":"Clothes",
-        "storeTimings": ["mon-Fri-10 to 10 pm", 
-            "sat-Sun-10 to 6 pm"
-        ],
-        "storeContact": {
-            "phone":"+91 567 234 890",
-            "address": "23-44-Near junction road"
-        },
-        "storeMapLocation":{
-            "latitude":98.0132,
-            "longitude":48.0965
-        }
     }
     ];
 
@@ -81,7 +137,7 @@ function StoreList(props) {
     const storeTemplate = (store) => {
        
         return (
-            <div className="flex-column border-500 surface-overlay border-2 border-round p-3">
+            <div className="flex-column border-700 surface-overlay border-2 border-round p-1 text-blue-800 text-xl">
                 <div className="flex column align-items-center w-8rem">
                     {store.storeName}
                 </div>
@@ -98,7 +154,7 @@ function StoreList(props) {
     return(
         <>
         <span className="title"/>
-        <InputText value={'wollong'} className=' text-white-alpha-90 bg-pink-800 m-4'/>
+        <InputText value={'wollong'} className=' text-black-alpha-90 text-xl  border-700 surface-overlay border-2 border-round m-4'/>
         <div className="card xl:flex xl:justify-content-center">
             <ListBox options={storeDirectory}
             onChange={(e) => setSelectedStore(e.value)}
