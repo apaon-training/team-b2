58fd9f3897dbfc0fee3f06ce01cd85c37d684e07
Rizwana-apaon <s.rizwana.apaon@gmail.com>
2024-08-05 10:02:45 +0000

    changes

--- rizwana-app/src/store-map/Store-Map.js
+++ rizwana-app/src/store-map/Store-Map.js
@@ -1,17 +1,55 @@
+import { Map,Marker } from "@vis.gl/react-google-maps";
+import { APIProvider } from "@vis.gl/react-google-maps";
+import  { useState } from 'react';
+
+
+
 
 
 function StoreMap(props) {
 
-    return(
+    const googleKey = "AIzaSyDSSP_p2LwfsNGP5eNXCd3cImC7Kf4RhBo";
+const [markerLocation, setMarkerLocation] = useState({
+
+    lat: props?.storeObj?.storeMapLocator?.Lattitudes,
+
+    lng:props?.storeObj?.storeMapLocator?.Longitudes});
+
+
+    return (
         <>
-        <div >
-        <div className="flex mr-4 h-4rem text-4xl text-blue-600">Store Map </div>
+        <div className="flex-column" >
+        {/* <div className="flex mr-4 h-4rem text-4xl text-blue-600">Store Map </div>
             <span className="flex align-items-center justify-content-center">{props?.storeObj?.storeMapLocator?.Lattitudes}</span>
             <span className="flex align-items-center justify-content-center">{props?.storeObj?.storeMapLocator?.Longitudes}</span>
+            <span></span> */}
+            <APIProvider apiKey={googleKey}>
+
+<Map
+
+  style={{ borderRadius: "20px" }}
+
+  defaultZoom={13}
+
+  defaultCenter={markerLocation}
+
+  gestureHandling={"greedy"}
+
+  disableDefaultUI
+
+>
+
+<Marker position={markerLocation} />
+
+</Map>
+
+</APIProvider>
+
+
            
         </div>
         </>
-    )
+    );
 
     
 }
