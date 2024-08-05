3c0c4f7987c842ab6e538c4744805755ea6c1213
Rizwana-apaon <s.rizwana.apaon@gmail.com>
2024-08-05 09:10:39 +0000

    changes

--- rizwana-app/src/store-map/Store-Map.js
+++ rizwana-app/src/store-map/Store-Map.js
@@ -5,8 +5,8 @@ function StoreMap(props) {
     return(
         <>
         <div >
-        <div className="flex mr-4 h-4rem text-4xl  text-blue-600">Store Map </div>
-            <span className="flex align-items-center justify-content-center ">{props?.storeObj?.storeMapLocator?.Lattitudes}</span>
+        <div className="flex mr-4 h-4rem text-4xl text-blue-600">Store Map </div>
+            <span className="flex align-items-center justify-content-center">{props?.storeObj?.storeMapLocator?.Lattitudes}</span>
             <span className="flex align-items-center justify-content-center">{props?.storeObj?.storeMapLocator?.Longitudes}</span>
            
         </div>
