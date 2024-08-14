61febd080b832de981defbc89410f58fed8e0975
saisree-apaon <v.saisree.apaon@gmail.com>
2024-08-14 08:42:11 +0000

    changes done

--- saisree-app/src/store-Form/storeForm.js
+++ saisree-app/src/store-Form/storeForm.js
@@ -1,6 +1,8 @@
 import { Dialog } from 'primereact/dialog';
 import { useState } from 'react';
 import './storeForm.css';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
 function StoreForm(props){
 
      const [visible, setVisible] = useState(false);
@@ -9,9 +11,84 @@ function StoreForm(props){
         <>
         <div className="card flex justify-content-center">
         <Dialog header="About" visible={props.visible} style={{ width: '50vw', height:"50vh"}} onHide={() =>  {props.onClose(false)}}>
-                <p className="m-0 mb-2 font-bold text-1xl">
-                    Show Store
+                <p className="m-0 mb-2 gap-2 font-bold text-3xl">
+                Add Store
                 </p>
+                <div className=' flex gap-2 text-xl my-3'>
+                <div className='pl-2'>
+                        Name:
+                        </div>
+                        <div className=''>
+                            <input></input>
+                        </div>
+                        </div>
+                        <div className='flex gap-2 text-xl my-3'>
+                        <div className='pl-2'>
+                        Details:
+                        </div>
+                        <div className=''>
+                            <input></input>
+                        </div>
+                        </div>
+                        <div className='flex gap-2 text-xl   my-3'>
+                        <div className='pl-2'>
+                        Timings1:
+                        </div>
+                        <div className=''>
+                            <input></input>
+                        </div>
+                        </div>
+                        <div className='flex gap-2 text-xl   my-3'>
+                        <div className='pl-2'>
+                        Timings2:
+                        </div>
+                        <div className=''>
+                            <input></input>
+                        </div>
+                        </div>
+                        <div className='flex gap-2 text-xl   my-3'>
+                        <div className='pl-2'>
+                        Phone:
+                        </div>
+                        <div className=''>
+                            <input></input>
+                        </div>
+                        </div>
+                        <div className='flex gap-2 text-xl   my-3'>
+                        <div className='pl-2'>
+                        Address:
+                        </div>
+                        <div className=''>
+                            <input></input>
+                        </div>
+                        </div>
+                        <div className='flex gap-2 text-xl   my-3'>
+                        <div className='pl-2'>
+                        Lattitude:
+                        </div>
+                        <div className=''>
+                            <input></input>
+                        </div>
+                        </div>
+                        <div className='flex gap-2 text-xl   my-3'>
+                        <div className='pl-2'>
+                        Longitude:
+                        </div>
+                        <div className=''>
+                            <input></input>
+                        </div>
+                        </div>
+                        <div>
+                            <div className=''>
+                                <button>Cancel</button>
+                            </div>
+                        </div>
+                        <div>
+                            <div className=''>
+                                <button>Submit</button>
+                            </div>
+                        </div>
+    
                 <p className="m-0 mb-2 animation-fill-forwards">
                    
                 </p> 
