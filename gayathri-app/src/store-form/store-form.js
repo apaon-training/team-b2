400dd73d47bf2cbec6d898d73e1409b4ec6677d5
gayathrikuruva <k.gayathri.apaon@gmail.com>
2024-08-15 09:37:42 +0000

    changes

--- gayathri-app/src/store-form/store-form.js
+++ gayathri-app/src/store-form/store-form.js
@@ -1,84 +1,89 @@
 import { Dialog } from 'primereact/dialog';
 import './store-form.css';
 import { InputText } from 'primereact/inputtext';
-import { useState } from 'react';
+import { Button } from 'primereact/button';
+import { InputNumber } from 'primereact/inputnumber';
 function StoreForm (props) {
+    const setName = (value) =>{}
+    const setDetails = (value) =>{}
+    const setPhone = (value) =>{}
+    const setAddress = (value) =>{}
+    const setTimings1 = (value) =>{}
+    const setTimings2 = (value) =>{}
+    const setLatitude = (value) =>{}
+    const setLongitude = (value) =>{}
     return (
         <div className="card flex justify-content-center">
-            <Dialog header="Add store"  visible={props.visible} style={{ width: '65vh', height: '75vh'}} onHide={() => {props.onClose(false)}}>
-                <div className='flex-column  h-auto'>
-                </div>
-                <div className='flex gap-6 h-1rem my-3'>
+            <Dialog header="Add store"  visible={props.visible} style={{ width: '70vh', height: '100vh'}} onHide={() => {props.onClose(false)}}>
+                <div className='flex-column'>
+                <div className='flex gap-6 mb-2 mt-4'>
                     <div className=''>
                         Name    :
                     </div>
-                    <div className=''>
-                        <input></input>
-                    </div>
+            <div className="">
+            <InputText value={Text} onChange={(e) => setName(e.target.value)} />
+            </div>
+            </div>
                 </div>
-                <div className='flex gap-6 h-2rem my-3'>
+                <div className='flex gap-6 mb-4'>
                     <div className=''>
                         Details :
                     </div>
                     <div className=''>
-                        <input></input>
+                    <InputText value={Text} onChange={(e) => setDetails(e.target.value)} />
                     </div>
                 </div>
-                <div className='flex gap-5 h-1rem my-3'>
+                <div className='flex gap-5 mb-2'>
                     <div className=''>
                         Timings1 :
                     </div>
                     <div className=''>
-                        <input></input>
+                    <InputText value={Text} onChange={(e) => setTimings1(e.target.value)} />
                     </div>
                 </div>
-                <div className='flex gap-5 h-2rem my-3'>
+                <div className='flex gap-5 mb-4'>
                     <div className=''>
                         Timings2 :
                     </div>
                     <div className=''>
-                        <input></input>
+                    <InputText value={Text} onChange={(e) => setTimings2(e.target.value)} />
                     </div>
                 </div>
-                <div className='flex gap-6 h-1rem my-3'>
+                <div className='flex gap-6 mb-2'>
                     <div className=''>
                         Phone    :
                     </div>
                     <div className=''>
-                        <input></input>
+                    <InputNumber value={Number} onChange={(e) => setPhone(e.target.value)} />
                     </div>
                 </div>
-                <div className='flex gap-5 h-2rem my-3'>
+                <div className='flex gap-5 mb-4'>
                     <div className=''>
                         Address  :
                     </div>
                     <div className=''>
-                        <input></input>
+                    <InputText value={Text} onChange={(e) => setAddress(e.target.value)} />
                     </div>
                 </div>
-                <div className='flex gap-5 h-1rem my-3'>
+                <div className='flex gap-5 mb-2'>
                     <div className=''>
                         Latitude  :
                     </div>
                     <div className=''>
-                        <input></input>
+                    <InputNumber value={Number} onChange={(e) => setLatitude(e.target.value)} />
                     </div>
                 </div>
-                <div className='flex gap-4 my-3'>
+                <div className='flex gap-4 mb-4'>
                     <div className=''>
                         Longitude :
                     </div>
                     <div className=''>
-                        <input></input>
+                    <InputNumber value={Number} onChange={(e) => setLongitude(e.target.value)} />
                     </div>
                 </div>
-                <div className='flex align-items-center justify-content-center gap-5 md:h-5rem'>
-                    <button className='ml-8 '>
-                        Cancel 
-                    </button>
-                    <button className='bg-blue-900 text-white'>
-                        Save 
-                    </button>
+                <div className='flex align-items-center justify-content-end gap-5'>
+                <Button label="Cancel" severity="secondary" outlined size='small'/>
+                <Button label="Save" severity="info" raised size='small'/> 
                 </div>
             </Dialog>
         </div>
