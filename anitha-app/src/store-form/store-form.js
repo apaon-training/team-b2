7d0b77e50e0f51af291476d959cf10e697768a3a
anitha-apaon <anitha.apaon@gmail.com>
2024-08-15 04:18:04 +0000

    add

--- anitha-app/src/store-form/store-form.js
+++ anitha-app/src/store-form/store-form.js
@@ -3,6 +3,7 @@ import { props } from 'primereact/utils';
 import { useState } from 'react';
 import './store-form.css';
 import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
 function StoreForm(props) {
     const [visible,setVisible]=useState(false);
     const setName=(value)=>{
@@ -29,10 +30,20 @@ function StoreForm(props) {
     const setLongitude=(value)=>{
         setLongitude(value);
     }
+    const saveClicked = () =>{
+    {
+            console.log('Login successful !');
+
+        }
+    }
+        const cancelClicked =() =>{
+            
+            console.log('cancel Clicked');
+        }
     return (
         <>
         <div className='card flex justify-content-center bg-white'>
-       <Dialog header=" Add Store" visible={props.visible} style={{ width: '50vw', height: '50vh'}} onHide={() => {props.onClose(false)}}>
+       <Dialog header=" Add Store" visible={props.visible} style={{ width: '50vw', height: '250vh'}} onHide={() => {props.onClose(false)}}>
        <div className='flex-column h-screen'>
         <div className='flex h-10rem text-4xl ml-3'>
             <span>
@@ -40,7 +51,7 @@ function StoreForm(props) {
             </span>
         </div>
        </div>
-            <div className='flex align-item-center justify-content-center gap-4 my-3'>
+            <div className='flex gap-6 my-3'>
                 <div className='Name'>
                     Name:
                 </div>
@@ -48,7 +59,7 @@ function StoreForm(props) {
                     <InputText value={ Text } onChange={(e) => setName (e.target.value)}/>
                 </div>
             </div>
-            <div className='flex align-item-center justify-content-center gap-4 my-3'>
+            <div className='flex gap-6 my-3'>
                 <div className='Details'>
                     Details:
                 </div>
@@ -56,13 +67,57 @@ function StoreForm(props) {
                     <InputText value={ Text } onChange={(e) => setDetails (e.target.value)}/>
                 </div>
             </div>
-            <div className='flex align-item-center justify-content-center gap-4 my-3'>
+            <div className='flex gap-5 my-3'>
                 <div className='Timings1'>
                     Timings1:
                 </div>
                 <div className=''>
                     <InputText value={ Text } onChange={(e) => setTiming1 (e.target.value)}/>
                 </div>
+            </div>
+            <div className='flex gap-5 my-3'>
+                <div className='Timings2'>
+                    Timings2:
+                </div>
+                <div className=''>
+                    <InputText value={ Text } onChange={(e) => setTiming2 (e.target.value)}/>
+                </div>
+            </div>
+            <div className='flex gap-6 my-3'>
+                <div className='Phone'>
+                    Phone:
+                </div>
+                <div className=''>
+                    <InputText value={ Text } onChange={(e) => setPhone (e.target.value)}/>
+                </div>
+            </div>
+            <div className='flex gap-5 my-3'>
+                <div className='Address'>
+                    Address:
+                </div>
+                <div className=''>
+                    <InputText value={ Text } onChange={(e) => setAddress (e.target.value)}/>
+                </div>
+            </div>
+            <div className='flex gap-5 my-3'>
+                <div className='Latitude'>
+                   Latitude :
+                </div>
+                <div className=''>
+                    <InputText value={ Text } onChange={(e) => setLatitude (e.target.value)}/>
+                </div>
+            </div>
+            <div className='flex gap-5 my-3'>
+                <div className='Longitude'>
+                    Longitude:
+                </div>
+                <div className=''>
+                    <InputText value={ Text } onChange={(e) => setLongitude (e.target.value)}/>
+                </div>
+            </div>
+            <div className="flex text-xl gap-3 mr-3 ">
+            <Button label="Cancel" color='white' size='small' onClick={() => cancelClicked()} />
+            <Button label="Save" size='small' onClick={() => saveClicked()} />
             </div>
             </Dialog>
             </div>
